9258dd0c7a3361f43fd02c90df3b5348847c17ce
DallasCarraher <dallas.carraher@gmail.com>
2021-05-10 13:51:31 -0700

    that's a promise!

--- src/api/fetchBooks.js
+++ src/api/fetchBooks.js
@@ -2,7 +2,7 @@ import request, { ROOT } from "./fetchWrapper";
 
 export const fetchBooks = async () => {
   const res = (await request(`${ROOT}/book`)).json();
-  const { docs, page, pages, total } = res;
+  const { docs, page, pages, total } = await res;
   const transformation = { docs, page, pages, total };
   return transformation;
 };
